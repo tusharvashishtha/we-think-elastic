a092a284753bce75f81d418382dd23d3fc6b6214
Siddharth6511 <vashishthatushar004@gmail.com>
2025-06-30 17:23:32 +0530

    page1

--- src/main.js
+++ src/main.js
@@ -2,15 +2,29 @@ import './style.css'
 import gsap from 'gsap'
 
 const images = document.querySelectorAll('.loaderImg');
-const tl = gsap.timeline({ repeat: -1 });
+const tl = gsap.timeline();
 
-images.forEach((img) => {
-  tl.to(img, {
-    opacity: 1,
-    duration: 0.1,
-  })
-  .to(img, {
-    opacity: 0,
-    duration: 0.1,
-  });
-});
+// images.forEach((img) => {
+//   tl.to(img, {
+//     opacity: 1,
+//     duration: 0.1,
+//   })
+//   .to(img, {
+//     opacity: 0,
+//     duration: 0.1,
+//   })
+// });
+
+// tl.to(images, {
+//   height: 0,
+//   duration: 0.1,
+//   ease: 'power1.inOut'
+// })
+
+// tl.to("#loader", {
+//   height: 0,
+//   duration: 1,
+//   delay: 0.2,
+//   ease: 'power1.inOut'
+// });
+
