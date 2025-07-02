0ebbad4ff07bd626f96a65a3a7a686f6ecd57361
Siddharth6511 <vashishthatushar004@gmail.com>
2025-07-03 01:27:23 +0530

    d

--- src/main.js
+++ src/main.js
@@ -8,29 +8,29 @@ import 'remixicon/fonts/remixicon.css'
 const images = document.querySelectorAll('.loaderImg');
 const tl = gsap.timeline();
 
-// images.forEach((img) => {
-//   tl.to(img, {
-//     opacity: 1,
-//     duration: 0.1,
-//   })
-//   .to(img, {
-//     opacity: 0,
-//     duration: 0.1,
-//   })
-// });
+images.forEach((img) => {
+  tl.to(img, {
+    opacity: 1,
+    duration: 0.1,
+  })
+  .to(img, {
+    opacity: 0,
+    duration: 0.1,
+  })
+});
 
-// tl.to(images, {
-//   height: 0,
-//   duration: 0.1,
-//   ease: 'power1.inOut'
-// })
+tl.to(images, {
+  height: 0,
+  duration: 0.1,
+  ease: 'power1.inOut'
+})
 
-// tl.to("#loader", {
-//   height: 0,
-//   duration: 1,
-//   delay: 0.2,
-//   ease: 'power1.inOut'
-// });
+tl.to("#loader", {
+  height: 0,
+  duration: 1,
+  delay: 0.2,
+  ease: 'power1.inOut'
+});
 
 tl.to(".hello", {
    translateY: 1,
@@ -97,35 +97,6 @@ gsap.to("#page8 h1", {
 })
 
 
-
-// document.addEventListener('mousemove', function(e) {
-//   const arrowBtn = document.getElementById('cursor');
-//   arrowBtn.style.left = e.clientX + 'px';
-//   arrowBtn.style.top = e.clientY + 'px';
-//   arrowBtn.animate({
-//     left: `${e.clientX}px`,
-//     top: `${e.clientY}px`
-//   }, {duration: 500, fill: "forwards"})
-// });
-// const videoContainer = document.getElementById('videoContainer');
-// const cursor = document.getElementById('cursor');
-
-// videoContainer.addEventListener('mouseenter', () => {
-//   cursor.classList.remove('hidden');
-// });
-
-// videoContainer.addEventListener('mouseleave', () => {
-//   cursor.classList.add('hidden');
-// });
-
-// videoContainer.addEventListener('mousemove', (e) => {
-//   const rect = videoContainer.getBoundingClientRect();
-//   const x = e.clientX - rect.left;
-//   const y = e.clientY - rect.top;
-
-//   cursor.style.left = `${rect.left + x}px`;
-//   cursor.style.top = `${rect.top + y}px`;
-//   
 
 
 const cursorDivs = document.querySelectorAll('.cursordiv');
