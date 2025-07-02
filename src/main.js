72ee7431bbb1cbe7d2db764030b2fe5df6488282
Siddharth6511 <vashishthatushar004@gmail.com>
2025-07-03 01:17:26 +0530

    complete

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import './style.css'
 import gsap from 'gsap'
 import ScrollTrigger from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
+import 'remixicon/fonts/remixicon.css'
 
 
 const images = document.querySelectorAll('.loaderImg');
@@ -66,17 +67,112 @@ gsap.to(".page4Svg2", {
   repeat: -1
 })
 
-// gsap.to("#page5 h1", {
-//   transform: "translateX(-150%)",
-//   duration: 4,
-//   scrollTrigger: {
-//     trigger: "#page5",
-//     scroller: "body",
-//     markers: true,
-//     start: "top -10%",
-//     end: "top -350%",
-//     scrub: 2,
-//     pin: true
-//   }
-// })
+gsap.to("#page5 h1", {
+  transform: "translateX(-150%)",
+  duration: 4,
+  scrollTrigger: {
+    trigger: "#page5",
+    scroller: "body",
 
+    start: "top -10%",
+    end: "top -350%",
+    scrub: 2,
+    pin: true
+  }
+})
+
+gsap.to("#page8 h1", {
+  translateY: 1,
+ duration: 0.5,
+
+
+    scrollTrigger: {
+      trigger: "#page8 h1",
+      scroller: "body",
+      // markers: true,
+      start: "top 100%",
+      end: "top 45%",
+      scrub: 2,
+    }
+})
+
+
+
+// document.addEventListener('mousemove', function(e) {
+//   const arrowBtn = document.getElementById('cursor');
+//   arrowBtn.style.left = e.clientX + 'px';
+//   arrowBtn.style.top = e.clientY + 'px';
+//   arrowBtn.animate({
+//     left: `${e.clientX}px`,
+//     top: `${e.clientY}px`
+//   }, {duration: 500, fill: "forwards"})
+// });
+// const videoContainer = document.getElementById('videoContainer');
+// const cursor = document.getElementById('cursor');
+
+// videoContainer.addEventListener('mouseenter', () => {
+//   cursor.classList.remove('hidden');
+// });
+
+// videoContainer.addEventListener('mouseleave', () => {
+//   cursor.classList.add('hidden');
+// });
+
+// videoContainer.addEventListener('mousemove', (e) => {
+//   const rect = videoContainer.getBoundingClientRect();
+//   const x = e.clientX - rect.left;
+//   const y = e.clientY - rect.top;
+
+//   cursor.style.left = `${rect.left + x}px`;
+//   cursor.style.top = `${rect.top + y}px`;
+//   
+
+
+const cursorDivs = document.querySelectorAll('.cursordiv');
+
+cursorDivs.forEach(div => {
+  const cursor = div.querySelector('.cursor');
+
+  cursor.classList.add('hidden');
+
+  div.addEventListener('mouseenter', () => {
+    cursor.classList.remove('hidden');
+  });
+
+  div.addEventListener('mouseleave', () => {
+    cursor.classList.add('hidden');
+  });
+
+  div.addEventListener('mousemove', (e) => {
+    const rect = div.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const offsetX = 25;
+    const offsetY = 25;
+
+    cursor.style.left = `${x - offsetX}px`;
+    cursor.style.top = `${y - offsetY}px`;
+    
+
+  });
+});
+
+
+
+let lastScrollY = window.scrollY;
+
+window.addEventListener('scroll', function () {
+  const navBar = document.getElementById('navbar');
+  const currentScrollY = window.scrollY;
+
+  if (currentScrollY > lastScrollY) {
+   
+    navBar.style.transform = 'translateY(-100%)';
+  } else {
+  
+    navBar.style.transform = 'translateY(0)';
+  }
+
+  lastScrollY = currentScrollY;
+});
